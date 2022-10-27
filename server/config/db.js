f2c68889082b944dbe4a8830320eaa94805a4082
wingshanip <wip12@my.centennialcollege.ca>
2022-10-27 11:13:46 -0400

    update db

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://127.0.0.1:27017/book_store"
-  //"URI": "mongodb+srv://wingssi:Rtdd7LXm8y9w5YeI@mongodbserver01.erbd0zg.mongodb.net/book_store?retryWrites=true&w=majority"
+  //"URI": "mongodb://127.0.0.1:27017/book_store"
+  "URI": "mongodb+srv://wingssi:Rtdd7LXm8y9w5YeI@mongodbserver01.erbd0zg.mongodb.net/book_store?retryWrites=true&w=majority"
 };
